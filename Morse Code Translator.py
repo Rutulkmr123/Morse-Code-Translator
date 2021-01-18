dbb83bbcb32d618a7e5379413f392c3a319bc9d7
Rutulkmr123 <61276978+Rutulkmr123@users.noreply.github.com>
2021-01-18 17:41:13 +0530

    Add files via upload

---
+++ Morse Code Translator.py
@@ -0,0 +1,124 @@
+import os
+
+import sys
+
+from playsound import playsound
+
+import time
+
+import pyttsx3 as pyttsx
+
+
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+
+                  'C': '-.-.', 'D': '-..', 'E': '.',
+
+                  'F': '..-.', 'G': '--.', 'H': '....',
+
+                  'I': '..', 'J': '.---', 'K': '-.-',
+
+                  'L': '.-..', 'M': '--', 'N': '-.',
+
+                  'O': '---', 'P': '.--.', 'Q': '--.-',
+
+                  'R': '.-.', 'S': '...', 'T': '-',
+
+                  'U': '..-', 'V': '...-', 'W': '.--',
+
+                  'X': '-..-', 'Y': '-.--', 'Z': '--..',
+
+                  '1': '.----', '2': '..---', '3': '...--',
+
+                  '4': '....-', '5': '.....', '6': '-....',
+
+                  '7': '--...', '8': '---..', '9': '----.',
+
+                  '0': '-----', ', ': '--..--', '.': '.-.-.-',
+
+                  '?': '..--..', '/': '-..-.', '-': '-....-',
+
+                  '(': '-.--.', ')': '-.--.-'}
+
+
+def Txt_to_Morse():
+
+   txt = input('Enter text to Convert to Morse: ')
+
+   code = [MORSE_CODE_DICT[i.upper()] + ' ' for i in txt if i.upper() in MORSE_CODE_DICT.keys()]
+
+   morse =''.join(code)
+
+   print(morse)
+
+   for m in morse:
+
+       if m=='.':
+
+           playsound('C://Users//ASUS//Downloads//dit.wav')
+
+       elif m=='-':
+
+           playsound('C://Users//ASUS//Downloads//dah.wav')
+
+       else:
+
+           time.sleep(0.5)
+
+
+def Morse_to_Txt():
+
+   txt = input('Enter Morse to Convert to Text: ')
+
+   code = [k for i in txt.split() for k,v in MORSE_CODE_DICT.items() if i==v]
+
+   newtxt =''.join(code)
+
+   print(newtxt)
+
+   engine = pyttsx.init()
+
+   engine.say(newtxt)
+
+   engine.runAndWait()
+
+
+
+if __name__=="__main__":
+    
+
+    while True:
+
+        try:
+            
+            print('''\n1 - Convert Text to Morse \n2 - Convert Morse to text\n3 - Quit\n''')
+
+            selection = int(input('Select Your Choices'))
+
+            if selection == 1:
+
+                print(Txt_to_Morse())
+
+          
+
+            elif selection == 2:
+
+                print(Morse_to_Txt())
+
+           
+
+            elif selection == 3: 
+                os._exit(0)
+                sys.quit(0)
+          
+          
+
+           
+
+            else:
+
+                print('Wrong Selection, enter again')
+
+        except:
+            pass
+       
+
